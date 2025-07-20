fe6ae81faf66f24d37ab5de20c82100739e6aa80
Alihaile <36104691+Alihaile@users.noreply.github.com>
2025-07-20 21:26:11 +0300

    linting urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,26 +1,26 @@
-# Uncomment the imports before you add the code
 from django.urls import path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # Auth routes
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('register', views.registration, name='register'),
 
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.login_user, name='logout'),
-    path(route='register', view=views.registration, name='register'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    # Dealerships
+    path('get_dealers/', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_reviews'),
 
-    # path for dealer reviews view
+    # Reviews and Cars
+    path('add_review', views.add_review, name='add_review'),
+    path('get_cars', views.get_cars, name='get_cars'),
+]
 
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
